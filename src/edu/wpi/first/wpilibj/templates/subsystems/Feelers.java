dccec78710ff1ed7d8a7e278f0fad58f86b193e6
ruqe <16dnong@wpsstudents.org>
2014-09-05 14:32:46 -0400

    Created Repository
    Feeler Test

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Feelers.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Solenoid;
+/**
+ *
+ * @author 16dnong
+ */
+public class Feelers extends Subsystem{
+    Jaguar leftFeeler, rightFeeler;
+    Solenoid feelerSolenoid;
+    final boolean FEELERS_OUT = true;
+    final boolean FEELERS_IN = false;
+    
+    public Feelers(){
+        leftFeeler = new Jaguar(RobotMap.leftFeelerMotor);
+        rightFeeler = new Jaguar(RobotMap.rightFeelerMotor);
+        feelerSolenoid = new Solenoid(RobotMap.feelerSolenoid);
+    }
+      public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+      
+    public void extend(){
+        if(getState() == FEELERS_IN){
+           feelerSolenoid.set(FEELERS_OUT);
+        } else{
+            feelerSolenoid.set(FEELERS_IN);
+        }
+    }
+    public void retract(){
+        if(getState() == FEELERS_OUT){
+            feelerSolenoid.set(FEELERS_IN);
+        }else{
+            feelerSolenoid.set(FEELERS_OUT);
+        }
+    }
+     public boolean getState(){
+         return feelerSolenoid.get();
+         
+     } 
+}
